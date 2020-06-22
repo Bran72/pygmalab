ad5ef92707581d0520297a1613c59cfd48351548
Brandon Leininger <brandon.leininger@icloud.com>
2020-06-22 09:26:17 +0200

    Updating routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Vuex from 'vuex'
 import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
+Vue.use(Vuex)
 
   const routes = [
   {
@@ -17,6 +19,14 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Dashboard.vue')
   }
 ]
 
