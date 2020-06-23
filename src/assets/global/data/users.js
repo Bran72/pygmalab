f71d23c1cde0c019c12dee703eb26ddeb10e1c02
robingiraud <robin.giraud5@gmail.com>
2020-06-23 10:17:56 +0200

    Adding User data fixtures
    users.js in src/assets/global/data

---
+++ src/assets/global/data/users.js
@@ -0,0 +1,36 @@
+const users = [
+    {
+        id: 1,
+        name: 'Robin Giraud',
+        email: 'robin.giraud5@gmail.com',
+        imgUrl: 'http://placehold.it/40x40',
+        password: 'admin',
+        role: 'Admin'
+    },
+    {
+        id: 2,
+        name: 'Brandon Leininger',
+        email: 'brandon.leininger@icloud.com',
+        imgUrl: 'http://placehold.it/40x40',
+        password: 'admin',
+        role: 'Admin'
+    },
+    {
+        id: 3,
+        name: 'Gérard Dupont',
+        email: 'gégé@gmail.com',
+        imgUrl: 'http://placehold.it/40x40',
+        password: 'gege',
+        role: 'Freelance'
+    },
+    {
+        id: 4,
+        name: 'Anthony Gault',
+        email: 'antho.gault@msn.com',
+        imgUrl: 'http://placehold.it/40x40',
+        password: 'anthodu33',
+        role: 'Client'
+    },
+];
+
+export default users;
