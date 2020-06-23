83015396458c5c03048b350e402cea3aaeee1741
Brandon Leininger <brandon.leininger@icloud.com>
2020-06-23 17:00:17 +0200

    :lipstick: NewProject.vue ending style

--- src/components/Cards/NewProject.vue
+++ src/components/Cards/NewProject.vue
@@ -3,8 +3,8 @@
         <p class="title">Demande de modélisation</p>
         <p class="subtitle">Je crée ma demande de modélisation</p>
         <div class="content">
-            <router-link to="/">
-                <p>+</p>
+            <router-link class="more-btn" to="/">
+                +
             </router-link>
         </div>
     </div>
@@ -17,4 +17,24 @@
 </script>
 
 <style lang="scss" scoped>
+    .content {
+        display: flex;
+        height: 100%;
+        padding-bottom: 5%;
+
+        .more-btn {
+            display: flex;
+            width: 5rem;
+            height: 5rem;
+            justify-content: center;
+            align-items: center;
+            border-radius: 50%;
+            margin: auto;
+            font-size: 3rem;
+            background: #F3F6FF;
+            box-shadow: -18px -18px 30px rgba(255, 255, 255, 0.8), 18px 18px 30px rgba(209, 217, 230, 0.8);
+            font-weight: bold;
+            color: #000 !important;
+        }
+    }
 </style>
