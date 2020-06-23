9417fba9ade6acc80cd2d3a54ce1065870d02a03
robingiraud <robin.giraud5@gmail.com>
2020-06-23 10:59:38 +0200

    Adding categories fixtures

---
+++ src/assets/global/data/categories.js
@@ -0,0 +1,13 @@
+const categories = [
+    'Animaux',
+    'Nature',
+    'Véhicules',
+    'Informatique',
+    'Mobilier',
+    'Outillage',
+    'Autres',
+];
+
+const customCategories = [];
+
+export default [categories, customCategories];
